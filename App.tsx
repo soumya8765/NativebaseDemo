a973995ca8cc2c23298df95aeb921f6c775b2a99
soumya8765 <arpitasoumyashree@gmail.com>
2023-06-21 17:53:43 +0530

    First commit

---
+++ App.tsx
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { Button, NativeBaseProvider } from 'native-base';
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+const App = ()=>{
+  return(
+
+    <NativeBaseProvider>
+
+      <Text>Welcome</Text>
+      <Button variant="outline">Click me</Button>
+
+
+
+
+    </NativeBaseProvider>
+
+  );
+};
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
